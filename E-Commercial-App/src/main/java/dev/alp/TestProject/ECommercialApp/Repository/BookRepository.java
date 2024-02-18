3cfe9851be0bcc4029134b3e23750bfb30f2731a
alp <alpkarar0@gmail.com>
2024-02-18 15:26:06 +0300

    Backend Unit Tests added in

--- E-Commercial-App/src/main/java/dev/alp/TestProject/ECommercialApp/Repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.alp.TestProject.ECommercialApp.Repository;
-
-import dev.alp.TestProject.ECommercialApp.Model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-}
